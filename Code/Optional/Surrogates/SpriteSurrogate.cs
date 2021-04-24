1a65d0b2208be4a78fd11ab4c84dd84d5b9f4936
Jack Walker <jackmw94@gmail.com>
2021-04-24 18:26:20 +0100

    Split up surrogates file

---
+++ Code/Optional/Surrogates/SpriteSurrogate.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+using UnityEngine;
+
+public sealed class SpriteSurrogate : ISerializationSurrogate
+{
+    private const string SpriteNameId = "spriteName";
+    
+    public void GetObjectData(object obj, SerializationInfo info, StreamingContext context)
+    {
+        Sprite sprite = (Sprite) obj;
+        info.AddValue(SpriteNameId, sprite.name);
+        Texture2DSurrogate.AddTextureData(sprite.texture, ref info);
+    }
+
+    public object SetObjectData(object obj, SerializationInfo info, StreamingContext context, ISurrogateSelector selector)
+    {
+        Texture2D textureData = Texture2DSurrogate.GetTextureData(info);
+        Sprite sprite = Sprite.Create (textureData, new Rect(0,0,textureData.width, textureData.height), new Vector2(.5f, .5f));
+        sprite.name = (string)info.GetValue(SpriteNameId, typeof(string));
+        return sprite;
+    }
+}
---
+++ Code/Optional/Surrogates/Texture2DSurrogate.cs
@@ -0,0 +1,52 @@
+﻿using System.Runtime.Serialization;
+using UnityEngine;
+
+public sealed class Texture2DSurrogate : ISerializationSurrogate
+{
+    private const string TextureNameId = "textureName";
+    private const string TextureFormatId = "textureFormat";
+    private const string AnsioLevelId = "ansioLevel";
+    private const string TextureSizeId = "textureSize";
+    private const string TextureDataId = "textureData";
+
+    public void GetObjectData(object obj, SerializationInfo info, StreamingContext context)
+    {
+        Texture2D texture = (Texture2D) obj;
+        AddTextureData(texture, ref info);
+    }
+
+    public object SetObjectData(object obj, SerializationInfo info, StreamingContext context, ISurrogateSelector selector)
+    {
+        return GetTextureData(info);
+    }
+
+    public static void AddTextureData(Texture2D texture, ref SerializationInfo info)
+    {
+        info.AddValue(TextureNameId, texture.name);
+        info.AddValue(TextureFormatId, texture.format);
+        info.AddValue(AnsioLevelId, texture.anisoLevel);
+        info.AddValue(TextureSizeId,  new Vector2Int(texture.width, texture.height));
+        info.AddValue(TextureDataId, texture.GetRawTextureData());
+    }
+
+    public static Texture2D GetTextureData(SerializationInfo info)
+    {
+        byte[] textureData = (byte[]) info.GetValue(TextureDataId, typeof(byte[]));
+
+        string textureName = (string) info.GetValue(TextureNameId, typeof(string));
+        TextureFormat textureFormat = (TextureFormat) info.GetValue(TextureFormatId, typeof(TextureFormat));
+        int ansioLevel = (int) info.GetValue(AnsioLevelId, typeof(int));
+        Vector2Int textureSize = (Vector2Int) info.GetValue(TextureSizeId, typeof(Vector2Int));
+
+        Texture2D texture = new Texture2D(textureSize.x, textureSize.y, textureFormat, false)
+        {
+            name = textureName
+        };
+
+        texture.LoadRawTextureData(textureData);
+        texture.anisoLevel = ansioLevel;
+        texture.Apply();
+
+        return texture;
+    }
+}
