dd62cc83da0ec0c74f737c3031375fdc8ca8570f
Jack Walker <jackmw94@gmail.com>
2022-02-20 17:22:49 +0000

    Wait until yield instruction that includes a timeout

---
+++ Code/Core/WaitUntilWithTimeout.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityExtras.Code.Core
+{
+    public sealed class WaitUntilWithTimeout : CustomYieldInstruction
+    {
+        private readonly Func<bool> _predicate;
+        private readonly float _timeout;
+
+        public override bool keepWaiting => !_predicate() && Time.time < _timeout;
+
+        public WaitUntilWithTimeout(Func<bool> predicate, float timeout)
+        {
+            _predicate = predicate;
+            _timeout = Time.time + timeout;
+        }
+    }
+}
